d2abd45ae27d7d6ac0e0833f4eef1d4a4962b9ad
NvrHere <solomoncasual@gmail.com>
2018-11-18 20:17:58 +0800

    DB Back

--- DataAsGuard/Program.cs
+++ DataAsGuard/Program.cs
@@ -17,6 +17,7 @@ namespace DataAsGuard
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Run(new Editor.EditorForm());
         }
     }
 }
