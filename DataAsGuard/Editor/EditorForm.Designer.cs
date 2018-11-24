ed4c150a17f3bc1fa8363d5adcf8cd57bdfe8817
NvrHere <solomoncasual@gmail.com>
2018-11-24 20:02:09 +0800

    Ver2 - PictureViewer
    PictureVieweInPlace

--- DataAsGuard/Editor/EditorForm.Designer.cs
+++ DataAsGuard/Editor/EditorForm.Designer.cs
@@ -28,33 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.showPic = new System.Windows.Forms.PictureBox();
+            this.fileNameBox = new System.Windows.Forms.TextBox();
+            this.zoom = new System.Windows.Forms.TrackBar();
+            this.opnFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.showPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zoom)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // showPic
             // 
-            this.button1.Location = new System.Drawing.Point(270, 147);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(318, 99);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.showPic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.showPic.Location = new System.Drawing.Point(44, 118);
+            this.showPic.Name = "showPic";
+            this.showPic.Size = new System.Drawing.Size(1803, 880);
+            this.showPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.showPic.TabIndex = 2;
+            this.showPic.TabStop = false;
+            // 
+            // fileNameBox
+            // 
+            this.fileNameBox.Location = new System.Drawing.Point(417, 32);
+            this.fileNameBox.Name = "fileNameBox";
+            this.fileNameBox.Size = new System.Drawing.Size(403, 20);
+            this.fileNameBox.TabIndex = 3;
+            this.fileNameBox.TextChanged += new System.EventHandler(this.fileNameBox_TextChanged);
+            // 
+            // zoom
+            // 
+            this.zoom.Location = new System.Drawing.Point(417, 67);
+            this.zoom.Maximum = 100;
+            this.zoom.Name = "zoom";
+            this.zoom.Size = new System.Drawing.Size(372, 45);
+            this.zoom.TabIndex = 4;
+            this.zoom.Scroll += new System.EventHandler(this.zoom_Scroll);
+            // 
+            // opnFile
+            // 
+            this.opnFile.Location = new System.Drawing.Point(826, 32);
+            this.opnFile.Name = "opnFile";
+            this.opnFile.Size = new System.Drawing.Size(51, 20);
+            this.opnFile.TabIndex = 0;
+            this.opnFile.Text = "Open";
+            this.opnFile.UseVisualStyleBackColor = true;
+            this.opnFile.Click += new System.EventHandler(this.opnFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(357, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Filename";
             // 
             // EditorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1487, 720);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zoom);
+            this.Controls.Add(this.fileNameBox);
+            this.Controls.Add(this.showPic);
+            this.Controls.Add(this.opnFile);
             this.Name = "EditorForm";
             this.Text = "EditorForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.EditorForm_FormClosing);
+            this.Load += new System.EventHandler(this.EditorForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.showPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zoom)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox showPic;
+        private System.Windows.Forms.TextBox fileNameBox;
+        private System.Windows.Forms.TrackBar zoom;
+        private System.Windows.Forms.Button opnFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- DataAsGuard/Editor/EditorForm.cs
+++ DataAsGuard/Editor/EditorForm.cs
@@ -13,22 +13,74 @@ namespace DataAsGuard.Editor
 {
     public partial class EditorForm : Form
     {
+        Image imgOriginal;
         public EditorForm()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        Image Zoom(Image img, Size size)
         {
-            DbClass dbCon = new DbClass();
-            //string queryStr = "SELECT username FROM da_schema.Userinfo";
-            List<string> dataDisplay = new List<string>();
-            dataDisplay = dbCon.DbRetrieve("Userinfo", "username");
-            MessageBox.Show("Connection Open ! ");
-            foreach (string i in dataDisplay)
+            Bitmap bmp = new Bitmap(img, img.Width + (img.Width * size.Width / 100), img.Height + (img.Height * size.Height / 100));
+            Graphics g = Graphics.FromImage(bmp);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bmp;
+        }
+
+        // DB Stuff
+        //DbClass dbCon = new DbClass();
+        ////string queryStr = "SELECT username FROM da_schema.Userinfo";
+        //List<string> dataDisplay = new List<string>();
+        //dataDisplay = dbCon.DbRetrieve("Userinfo", "username");
+        //    MessageBox.Show("Connection Open ! ");
+        //    foreach (string i in dataDisplay)
+        //    {
+        //        MessageBox.Show(i);
+        //    }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void opnFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, ValidateNames = true, Filter = "JPEG|*.jpg" })
             {
-                MessageBox.Show(i);
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    showPic.Image = Image.FromFile(ofd.FileName);
+                    imgOriginal = showPic.Image;
+                }
             }
+        }
+
+        private void zoom_Scroll(object sender, EventArgs e)
+        {
+            if (zoom.Value > 0)
+            {
+                showPic.Image = Zoom(imgOriginal, new Size(zoom.Value, zoom.Value));
+            }
+        }
+
+        private void EditorForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (showPic.Image != null)
+            {
+                showPic.Dispose();
+            }
+        }
+
+        private void fileNameBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditorForm_Load(object sender, EventArgs e)
+        {
+            //this.TopMost = true;
+            //this.FormBorderStyle = FormBorderStyle.None;
+            //this.WindowState = FormWindowState.Maximized;
         }
     }
 }
