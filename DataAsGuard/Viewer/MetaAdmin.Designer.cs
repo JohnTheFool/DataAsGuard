1a8b705552150b5b306da4ffcddbc1490bd7e70e
JohnTheFool <johnfoohw@gmail.com>
2019-01-25 00:20:21 +0800

    Revert "Revert "Merge branch 'SoloTempBranch'""
    This reverts commit e0c4934943857c23748525cfdbb3d13bab5118ba.

---
+++ DataAsGuard/Viewer/MetaAdmin.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace DataAsGuard.Viewer
+{
+    partial class MetaAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.openFileBtn = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.backBtn = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(93, 26);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(121, 42);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(490, 20);
+            this.filePath.TabIndex = 1;
+            // 
+            // openFileBtn
+            // 
+            this.openFileBtn.Location = new System.Drawing.Point(631, 42);
+            this.openFileBtn.Name = "openFileBtn";
+            this.openFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.openFileBtn.TabIndex = 2;
+            this.openFileBtn.Text = "Open";
+            this.openFileBtn.UseVisualStyleBackColor = true;
+            this.openFileBtn.Click += new System.EventHandler(this.openFileBtn_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(121, 108);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(490, 276);
+            this.textBox.TabIndex = 3;
+            // 
+            // backBtn
+            // 
+            this.backBtn.Location = new System.Drawing.Point(22, 39);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(54, 23);
+            this.backBtn.TabIndex = 4;
+            this.backBtn.Text = "Back";
+            this.backBtn.UseVisualStyleBackColor = true;
+            this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
+            // 
+            // MetaAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.backBtn);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.openFileBtn);
+            this.Controls.Add(this.filePath);
+            this.Name = "MetaAdmin";
+            this.Text = "MetaAdmin";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Button openFileBtn;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button backBtn;
+    }
+}
---
+++ DataAsGuard/Viewer/MetaAdmin.cs
@@ -0,0 +1,140 @@
+﻿using Steganography;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataAsGuard.Viewer
+{
+    public partial class MetaAdmin : Form
+    {
+        public MetaAdmin()
+        {
+            InitializeComponent();
+        }
+        private Bitmap bmp = null;
+
+        private void readMeta(string fileName)
+        {
+            // Read File extended properties
+            List<string> arrHeaders = new List<string>();
+            List<Tuple<int, string, string>> attributes = new List<Tuple<int, string, string>>();
+
+            Shell32.Shell shell = new Shell32.Shell();
+            Shell32.Folder objFolder = shell.NameSpace(System.IO.Path.GetDirectoryName(fileName));
+            Shell32.FolderItem folderItem = objFolder.ParseName(System.IO.Path.GetFileName(fileName));
+
+
+            for (int i = 0; i < short.MaxValue; i++)
+            {
+                string header = objFolder.GetDetailsOf(null, i);
+                if (String.IsNullOrEmpty(header))
+                    break;
+                arrHeaders.Add(header);
+            }
+
+            // The attributes list below will contain a tuple with attribute index, name and value
+            // Once you know the index of the attribute you want to get, 
+            // you can get it directly without looping, like this:
+            var Authors = objFolder.GetDetailsOf(folderItem, 20);
+
+            for (int i = 0; i < arrHeaders.Count; i++)
+            {
+                var attrName = arrHeaders[i];
+                var attrValue = objFolder.GetDetailsOf(folderItem, i);
+                var attrIdx = i;
+
+                attributes.Add(new Tuple<int, string, string>(attrIdx, attrName, attrValue));
+                if (i <= 5 || i == 10 || i == 21 || i == 24 || i == 25 || i == 33)
+                {
+                    textBox.Text += attrName + ": " + attrValue + Environment.NewLine;
+                    //Console.WriteLine("{0}\t{1}: {2}", i, attrName, attrValue);
+                    Console.WriteLine("{0}\t{1}: {2}", i + 1, attrName, attrValue);
+                }
+            }
+        }
+
+        private void openFileBtn_Click(object sender, EventArgs e)
+        {
+            filePath.Clear();
+            textBox.Clear();
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Text Documents;Word Docx;PPTX;XLSX;Png;MP4;All |*.txt;*.docx;*pptx;*xlsx;*.png;*mp4;*.*";
+            if (open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath.Text = open_dialog.FileName;
+                var extension = Path.GetExtension(open_dialog.FileName);
+                switch (extension.ToLower())
+                {
+                    case ".docx":
+                        readMeta(open_dialog.FileName);
+                        break;
+                    case ".xlsx":
+                        readMeta(open_dialog.FileName);
+                        break;
+                    case ".pptx":
+                        readMeta(open_dialog.FileName);
+                        break;
+                    case ".png":
+                        string password = "Ilovethispic123";
+                        bmp = (Bitmap)Image.FromFile(open_dialog.FileName); ;
+                        string extractedText = SteganographyHelper.extractText(bmp);
+                        extractedText = Crypto.DecryptStringAES(extractedText, password);
+                        textBox.Text = extractedText;
+                        break;
+                    case ".txt":
+                        System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+
+                        pProcess.StartInfo.UseShellExecute = false;
+
+                        pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+
+                        //strCommand is path and file name of command to run
+                        pProcess.StartInfo.FileName = "cmd.exe";
+
+                        //strCommandParameters are parameters to pass to program
+                        pProcess.StartInfo.Arguments = "/C more < " + open_dialog.FileName + ":DS1";
+
+                        pProcess.StartInfo.UseShellExecute = false;
+
+                        //Set output of program to be written to process output stream
+                        pProcess.StartInfo.RedirectStandardOutput = true;
+
+                        //Optional
+                        //pProcess.StartInfo.WorkingDirectory = strWorkingDirectory;
+
+                        //Start the process
+                        pProcess.Start();
+
+                        //Get program output
+                        string strOutput = pProcess.StandardOutput.ReadToEnd();
+
+                        textBox.Text = strOutput;
+                        //Wait for process to finish
+                        pProcess.WaitForExit();
+                        break;
+                    case ".mp4":
+                        TagLib.File videoFile = TagLib.File.Create(open_dialog.FileName);
+                        TagLib.Mpeg4.AppleTag customTag = (TagLib.Mpeg4.AppleTag)videoFile.GetTag(TagLib.TagTypes.Apple);
+                        string tokenValue = customTag.GetDashBox("User", "Downloaded");
+                        textBox.Text = tokenValue;
+                        break;
+                    }
+                }
+            }
+
+        private void backBtn_Click(object sender, EventArgs e)
+        {
+            Home home = new Home();
+            home.Show();
+            Hide();
+        }
+    }
+}
